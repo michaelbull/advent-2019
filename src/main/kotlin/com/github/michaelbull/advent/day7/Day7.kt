e3256e950beca5675bf4cafffa2be9d1dc2f09bf
Michael Bull <mikebull94@gmail.com>
2019-12-07 18:16:55 +0000

    Avoid using buffered channels for sequential channels in thrusterSignalFeedbackLoop

--- src/main/kotlin/com/github/michaelbull/advent/day7/Day7.kt
+++ src/main/kotlin/com/github/michaelbull/advent/day7/Day7.kt
@@ -82,25 +82,35 @@ suspend fun Intcode.thrusterSignalFeedbackLoop(phaseSettings: IntArray): Int {
     val amplifiers = phaseSettings.toAmplifiers()
     val computers = amplifiers.associateWith { IntcodeComputer() }
 
-    for (i in 0 until amplifiers.size - 1) {
-        val curr = amplifiers[i]
-        val next = amplifiers[i + 1]
+    return coroutineScope {
+        for (i in 0 until amplifiers.size - 1) {
+            val curr = amplifiers[i]
+            val next = amplifiers[i + 1]
+            val channel = computers.channelBetween(curr, next)
+            addInputs(channel, next)
+        }
 
-        computers.pair(curr, next)
-    }
+        val first = amplifiers.first()
+        val last = amplifiers.last()
+        val feedbackChannel = computers.channelBetween(last, first, capacity = 1)
+        addInputs(feedbackChannel, first)
 
-    val loopChannel = computers.pair(
-        curr = amplifiers.last(),
-        next = amplifiers.first()
-    )
-
-    coroutineScope {
         for (computer in computers.values) {
             compute(computer, this@thrusterSignalFeedbackLoop)
         }
-    }
 
-    return loopChannel.receive()
+        feedbackChannel
+    }.receive()
+}
+
+private fun CoroutineScope.addInputs(channel: Channel<Int>, amplifier: Amplifier) = launch {
+    /* Provide each amplifier its phase setting at its first input instruction */
+    channel.send(amplifier.phaseSetting)
+
+    /* To start the process, a 0 signal is sent to amplifier A's input exactly once. */
+    if (amplifier.id == 'A') {
+        channel.send(0)
+    }
 }
 
 private fun CoroutineScope.compute(computer: IntcodeComputer, program: Intcode) = launch {
@@ -109,28 +119,13 @@ private fun CoroutineScope.compute(computer: IntcodeComputer, program: Intcode) 
     computer.compute()
 }
 
-private suspend fun Map<Amplifier, IntcodeComputer>.pair(curr: Amplifier, next: Amplifier): Channel<Int> {
-
-    /* All signals sent or received in this process will be between pairs */
-    val channel = Channel<Int>(capacity = 2)
-
-    /* Provide each amplifier its phase setting at its first input instruction */
-    channel.send(next.phaseSetting)
-
-    /* To start the process, a 0 signal is sent to amplifier A's input exactly once. */
-    if (next.id == 'A') {
-        channel.send(0)
-    }
-
-    getValue(curr).onOutput {
-        channel.send(it)
-    }
-
-    getValue(next).onInput {
-        channel.receive()
-    }
-
-    return channel
+private suspend fun Map<Amplifier, IntcodeComputer>.channelBetween(
+    curr: Amplifier,
+    next: Amplifier,
+    capacity: Int = Channel.RENDEZVOUS
+) = Channel<Int>(capacity).apply {
+    getValue(curr).onOutput { send(it) }
+    getValue(next).onInput { receive() }
 }
 
 fun main() = runBlocking {
