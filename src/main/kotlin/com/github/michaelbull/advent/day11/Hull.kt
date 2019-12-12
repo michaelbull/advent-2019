c884692e31e250054b9f335ca547874cb91d8773
Michael Bull <mikebull94@gmail.com>
2019-12-12 21:12:58 +0000

    Dedupe in Hull#prettyPrint

--- src/main/kotlin/com/github/michaelbull/advent/day11/Hull.kt
+++ src/main/kotlin/com/github/michaelbull/advent/day11/Hull.kt
@@ -14,17 +14,23 @@ class Hull {
     }
 
     fun prettyPrint(): String {
-        val xValues = panels.map { it.key.x }
-        val yValues = panels.map { it.key.y }
-        val x0 = xValues.min()!!
-        val x1 = xValues.max()!!
-        val y0 = yValues.min()!!
-        val y1 = yValues.max()!!
+        val (x0, x1) = panels.map { it.key.x }.bounds()
+        val (y0, y1) = panels.map { it.key.y }.bounds()
 
         return (y0..y1).map { y ->
             (x0..x1).map { x ->
                 this[Position(x, y)].toChar()
             }.joinToString(separator = "")
         }.reversed().joinToString(separator = "\n")
+    }
+
+    private fun Iterable<Int>.bounds(): Pair<Int, Int> {
+        return fold(EMPTY_BOUNDS) { (min, max), value ->
+            Pair(min.coerceAtMost(value), max.coerceAtLeast(value))
+        }
+    }
+
+    private companion object {
+        private val EMPTY_BOUNDS = Pair(Int.MAX_VALUE, Int.MIN_VALUE)
     }
 }
